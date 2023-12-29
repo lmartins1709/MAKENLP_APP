564d197e3bfa9407f12b04c207e30688b1d1ee40
Lucas Alves Martins <129783415+lmartins1709@users.noreply.github.com>
2023-12-29 15:12:00 -0300

    Update generation.py

--- tcc/generation.py
+++ tcc/generation.py
@@ -1,10 +1,10 @@
 from transformers import AutoModelForQuestionAnswering, AutoTokenizer
 import torch
 import os
+model_name = 'pierreguillou/bert-base-cased-squad-v1.1-portuguese'
+model = AutoModelForQuestionAnswering.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 def generate_answer(question, context):
-    model_name = 'pierreguillou/bert-base-cased-squad-v1.1-portuguese'
-    model = AutoModelForQuestionAnswering.from_pretrained(model_name)
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
     inputs = tokenizer(question, context, return_tensors="pt")
     outputs = model(**inputs)
     answer_start = torch.argmax(outputs.start_logits)
