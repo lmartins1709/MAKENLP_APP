116e5742a861561e4ab5d70e0d2d90bfae92da4f
Lucas Alves Martins <129783415+lmartins1709@users.noreply.github.com>
2023-12-29 16:36:46 -0300

    Update generation.py

--- tcc/generation.py
+++ tcc/generation.py
@@ -1,13 +1,26 @@
 from transformers import AutoModelForQuestionAnswering, AutoTokenizer
 import torch
-import os
-model_name = 'pierreguillou/bert-base-cased-squad-v1.1-portuguese'
-model = AutoModelForQuestionAnswering.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-def generate_answer(question, context):
-    inputs = tokenizer(question, context, return_tensors="pt")
-    outputs = model(**inputs)
-    answer_start = torch.argmax(outputs.start_logits)
-    answer_end = torch.argmax(outputs.end_logits) + 1
-    answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(inputs["input_ids"][0][answer_start:answer_end]))
-    return answer
+
+class QuestionAnsweringModel:
+    def __init__(self, model_name):
+        self.model_name = model_name
+        self.model = None
+        self.tokenizer = None
+
+    def load_model(self):
+        if self.model is None:
+            self.model = AutoModelForQuestionAnswering.from_pretrained(self.model_name)
+            self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+
+    def generate_answer(self, question, context):
+        if self.model is None:
+            raise ValueError("Model not loaded. Call load_model() first.")
+
+        inputs = self.tokenizer(question, context, return_tensors="pt")
+        outputs = self.model(**inputs)
+        answer_start = torch.argmax(outputs.start_logits)
+        answer_end = torch.argmax(outputs.end_logits) + 1
+        answer = self.tokenizer.convert_tokens_to_string(
+            self.tokenizer.convert_ids_to_tokens(inputs["input_ids"][0][answer_start:answer_end])
+        )
+        return answer
